11a7578d9ad8a1d87dc2bbba4b851d88e5d39ddc
Sofiia-Solomiia <sofiia-solomiia.pavlyshyn.mitpa.2024@lpnu.ua>
2025-05-13 17:39:14 +0300

    Update demo.tf

--- demo.tf
+++ demo.tf
@@ -32,7 +32,7 @@ resource "aws_subnet" "demosubnet" {
   vpc_id                  = "${aws_vpc.demovpc.id}"
   cidr_block             = "${var.subnet_cidr}"
   map_public_ip_on_launch = true
-  availability_zone = "us-east-1a"
+  availability_zone = "eu-north-1a"
 
   tags = {
     Name = "Demo subnet"
@@ -44,7 +44,7 @@ resource "aws_subnet" "demosubnet1" {
   vpc_id                  = "${aws_vpc.demovpc.id}"
   cidr_block             = "${var.subnet1_cidr}"
   map_public_ip_on_launch = true
-  availability_zone = "us-east-1b"
+  availability_zone = "eu-north-1b"
 
   tags = {
     Name = "Demo subnet 1"
