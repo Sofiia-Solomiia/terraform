3b05d6b5c2e979b9f6c1fd675f294089fc2bd849
Sofiia-Solomiia <sofiia-solomiia.pavlyshyn.mitpa.2024@lpnu.ua>
2025-05-13 17:56:40 +0300

    Update demo.tf

--- demo.tf
+++ demo.tf
@@ -184,15 +184,13 @@ resource "aws_instance" "demoinstance1" {
   # Installing splunk & creating distributed indexer clustering on newly created instance
   provisioner "remote-exec" {
     inline = [
-      "sudo yum update -y",
-      "sudo amazon-linux-extras install docker -y",
-      "sudo service docker start",
-      "sudo usermod -a -G docker ec2-user",
-      "sudo chkconfig docker on",
-      "sudo yum install -y git",
+      "sudo apt-get update -y",
+      "sudo apt-get install -y docker.io git",
+      "sudo systemctl start docker",
+      "sudo usermod -aG docker ${self.tags.Name}",
       "sudo chmod 666 /var/run/docker.sock",
       "docker pull dhruvin30/dhsoniweb:v1",
-      "docker run -d -p 80:80 dhruvin30/dhsoniweb:v1"   
-  ]
- }
+      "docker run -d -p 80:80 dhruvin30/dhsoniweb:v1"
+    ]
+  }
 }
